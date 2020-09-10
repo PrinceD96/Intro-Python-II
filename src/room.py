cda9c05bad221b210ceed67f781c6042b21e4699
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-09-10 19:46:15 -0400

    :ambulance: Made rooms able to hold multiple items

--- src/room.py
+++ src/room.py
@@ -4,13 +4,17 @@ import textwrap
 
 
 class Room:
-    def __init__(self, name, description, n_to=None, e_to=None, s_to=None, w_to=None):
+    def __init__(self, name, description, items=None, n_to=None, e_to=None, s_to=None, w_to=None):
         self.name = name
         self.description = description
         self.n_to = n_to
         self.e_to = e_to
         self.s_to = s_to
         self.w_to = w_to
+        if items == None:
+            self.items = []
+        else:
+            self.items = items
 
     def print_room(self):
         wrapper = textwrap.TextWrapper(width=80)
